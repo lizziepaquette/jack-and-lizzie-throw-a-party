494bacaf2362c7c39177d1841863ef030c9b9bf4
Lizzie Paquette <lizzie@brex.com>
2023-05-08 22:54:24 -0700

    change background and update apostrophes

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sunset from '../assets/photos/rollerskates.jpeg'
+import LightBlueVelvet from '../assets/photos/light-blue-velvet.jpg'
 import styled from 'styled-components'
 
 
@@ -15,7 +15,7 @@ const InfoSection = ({ title, content }) => {
 const InfoWrapper = styled.div`
     color: white;
     padding: 70px 5% 5% 35px;
-    background-image: url(${Sunset});
+    background-image: url(${LightBlueVelvet});
     box-sizing: border-box;
     background-size: cover;
     background-repeat: no-repeat;
@@ -37,9 +37,9 @@ const Schedule = () => {
       title: '2pm - 4pm: The Coffee Movement',
       content: <div>
         <div>
-          One Saturday this past December, I went down the rabbit hole that is the US Coffee Championships. What did I find? 
-          One of 2022's Barista finalists owned a shop in SF!!<br /><br /> Their shop in the Richmond is beautiful, they get their beans 
-          from some of the world's best roasters, and if you tell the owner that you loved your coffee, he may respond with, "You deserve it"... 
+          One Saturday this past December, I went down the rabbit hole that is the US Coffee Championships. What did I find?
+          One of 2022&apos;s Barista finalists owned a shop in SF!!<br /><br /> Their shop in the Richmond is beautiful, they get their beans
+          from some of the world&apos;s best roasters, and if you tell the owner that you loved your coffee, he may respond with, &quot;You deserve it&quot;...
           Self-love highly encouraged!
         </div>
       </div>
@@ -48,8 +48,8 @@ const Schedule = () => {
       title: '4:15pm - 6:30pm: Roosevelt & Edison Escape Rooms @ Palace Games',
       content: <div>
             <div>
-              Our Escape Room-designing friend Matthew Stein doesn't just hand out room recommendations, but Palace Games' rooms were one of the 
-              3 places in the Bay that he touted! <br /><br /> Each room fits 4-8 solvers, so we'll divide up and have some fun!
+              Our Escape Room-designing friend Matthew Stein doesn&apos;t just hand out room recommendations, but Palace Games&apos; rooms were one of the
+              3 places in the Bay that he touted! <br /><br /> Each room fits 4-8 solvers, so we&apos;ll divide up and have some fun!
             </div>
         </div>
     },
@@ -57,7 +57,7 @@ const Schedule = () => {
       title: '6:45pm - 8:15pm: Pollo a la Brasa from Jaranitas',
       content: <div>
             <div>
-              May tweak this but... we'll step out of our escape rooms and into the lovely Palace of Fine Arts, toss down some park blankets on the grass, get 
+              May tweak this but... we&apos;ll step out of our escape rooms and into the lovely Palace of Fine Arts, toss down some park blankets on the grass, get
               Pollo a la Brasa (Peruvian roasted chicken) delivered from Jaranitas, break open beverages of our chosing, and recharge while the sun sets over the Golden Gate.
             </div>
         </div>
@@ -66,7 +66,7 @@ const Schedule = () => {
       title: '9pm - Whenever: Bar Part Time',
       content: <div>
             <div>
-              The natural wine... The dj sets... The fog... The disco ball...<br /><br /> 
+              The natural wine... The dj sets... The fog... The disco ball...<br /><br />
               <strong>WE ALL KNEW IT WAS COMING BUT I STILL WANT IT, DAMMIT!!</strong>
             </div>
         </div>
